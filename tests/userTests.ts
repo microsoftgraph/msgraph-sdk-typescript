567d1d5d6e67f4949e90eeab2901ae3a25ae405a
Nikitha Chettiar <nikithauc@gmail.com>
2022-04-01 01:59:32 -0700

    cleanup

--- tests/userTests.ts
+++ tests/userTests.ts
@@ -13,7 +13,6 @@ describe("TestGet", () => {
 
     it("should return messages for given user", async () => {
         const messages = await graphServiceClient.usersById("a4bf4e43-981f-430c-8024-cdb645f8c098").messages.get();
-        console.log(messages);
         assert.isDefined(messages?.value);
     });
 });
