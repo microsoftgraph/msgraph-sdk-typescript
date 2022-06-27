6e5c6b0c810f42bfbe1c436780213908fa88371d
Nikitha Chettiar <nikithauc@gmail.com>
2022-06-27 15:12:05 -0700

    restore

---
+++ src/graphclient/index.ts
@@ -0,0 +1,9 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
+export { SimpleAccessTokenProvider, SimpleAuthenticationProvider } from "@microsoft/microsoft-graph-client";
+export { GraphServiceClient } from "./graphServiceClient";
