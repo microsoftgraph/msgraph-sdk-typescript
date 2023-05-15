e8d44d178334013d94b0843c120d4501d7b0a350
Vincent Biret <vibiret@microsoft.com>
2023-05-15 09:01:54 -0400

    - updates unit test to latest indexer
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- tests/userTests.ts
+++ tests/userTests.ts
@@ -12,7 +12,7 @@ describe("TestGet", () => {
     });
 
     it("should return messages for given user", async () => {
-        const messages = await graphServiceClient.usersById("a4bf4e43-981f-430c-8024-cdb645f8c098").messages.get();
+        const messages = await graphServiceClient.users.byUserId("a4bf4e43-981f-430c-8024-cdb645f8c098").messages.get();
         assert.isDefined(messages?.value);
     });
 });
