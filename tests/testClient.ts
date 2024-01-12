b85c2fc4d9990ed7006bc199f7cf6f6b4d5581d1
Vincent Biret <vibiret@microsoft.com>
2024-01-12 15:07:09 -0500

    - removes dependency on core client

--- tests/testClient.ts
+++ tests/testClient.ts
@@ -1,9 +1,14 @@
-import {GraphServiceClient} from "../src/graphclient/index";
-import {AzureIdentityAuthenticationProvider} from "@microsoft/kiota-authentication-azure";
-import {ClientSecretCredential} from "@azure/identity";
+import { GraphBaseServiceClient } from "../src/graphBaseServiceClient";
+import { AzureIdentityAuthenticationProvider } from "@microsoft/kiota-authentication-azure";
+import { ClientSecretCredential } from "@azure/identity";
+import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
 
-const tokenCredential = new ClientSecretCredential(process.env.TENANT_ID, process.env.CLIENT_ID, process.env.CLIENT_SECRET );
+const tokenCredential = new ClientSecretCredential(
+  process.env.TENANT_ID,
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
+);
 
 const authProvider = new AzureIdentityAuthenticationProvider(tokenCredential);
-export const graphServiceClient = GraphServiceClient.init({authProvider});
-
+const requestAdapter = new FetchRequestAdapter(authProvider);
+export const graphServiceClient = new GraphBaseServiceClient(requestAdapter);
