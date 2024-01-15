37f66c1e974c8fc1c9be4f3dc2aae0f8404cc37d
Vincent Biret <vibiret@microsoft.com>
2024-01-15 11:20:03 -0500

    - restores configuration files

--- packages/msgraph-sdk-javascript/__tests__/msgraph-sdk-javascript.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const msgraphSdkJavascript = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(msgraphSdkJavascript(), 'Hello from msgraphSdkJavascript');
-console.info('msgraphSdkJavascript tests passed');
