ba1088772f3c5f4ba9d447d87d9ea874d4113837
Vincent Biret <vibiret@microsoft.com>
2024-01-15 11:12:10 -0500

    - adds main package

---
+++ packages/msgraph-sdk-javascript/README.md
@@ -0,0 +1,11 @@
+# `@microsoft/msgraph-sdk-javascript`
+
+> TODO: description
+
+## Usage
+
+```
+const msgraphSdkJavascript = require('@microsoft/msgraph-sdk-javascript');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/msgraph-sdk-javascript/__tests__/msgraph-sdk-javascript.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const msgraphSdkJavascript = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(msgraphSdkJavascript(), 'Hello from msgraphSdkJavascript');
+console.info('msgraphSdkJavascript tests passed');
