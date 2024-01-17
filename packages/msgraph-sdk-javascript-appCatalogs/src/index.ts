0e7eea36f16363456b514ed375f4f9515a563fb7
Vincent Biret <vibiret@microsoft.com>
2024-01-17 09:38:45 -0500

    - moves fluent api for app catalogs to the root
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/msgraph-sdk-javascript-appCatalogs/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { GraphServiceClient, extendGraphServiceClient } from "@microsoft/msgraph-sdk-javascript";
-import { AppCatalogsServiceClient, AppCatalogsServiceClientNavigationMetadata } from "./appCatalogsServiceClient";
-
-declare module "@microsoft/msgraph-sdk-javascript" {
-	interface GraphServiceClient extends AppCatalogsServiceClient {}
-}
-extendGraphServiceClient(AppCatalogsServiceClientNavigationMetadata);
