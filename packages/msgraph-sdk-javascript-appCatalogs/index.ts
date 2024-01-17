d641fe8bd61097bc59046ca6d7265f4d361c2fdc
Vincent Biret <vibiret@microsoft.com>
2024-01-17 13:11:03 -0500

    - adds some duplication so two APIs can be consumed
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/msgraph-sdk-javascript-appCatalogs/index.ts
+++ packages/msgraph-sdk-javascript-appCatalogs/index.ts
@@ -4,8 +4,16 @@ import {
   type AppCatalogsServiceClient,
   AppCatalogsServiceClientNavigationMetadata,
 } from "./appCatalogsServiceClient";
+import { AppCatalogsRequestBuilder } from "appCatalogs";
 
 declare module "@microsoft/msgraph-sdk-javascript" {
-  interface GraphServiceClient extends AppCatalogsServiceClient {}
+  // interface GraphServiceClient extends AppCatalogsServiceClient {}
+  interface GraphServiceClient {
+    /**
+     * Provides operations to manage the appCatalogs singleton.
+     */
+    get appCatalogs(): AppCatalogsRequestBuilder;
+  }
 }
 extendGraphServiceClient(AppCatalogsServiceClientNavigationMetadata);
+export * from "./appCatalogsServiceClient";
