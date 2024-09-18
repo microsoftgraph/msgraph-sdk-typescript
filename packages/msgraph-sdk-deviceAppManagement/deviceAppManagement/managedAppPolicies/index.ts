370d96866c45f5a1b5d40eb98feff79b754445ae
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-09-18 11:45:47 +0000

    Update generated files with build 163754

--- packages/msgraph-sdk-deviceAppManagement/deviceAppManagement/managedAppPolicies/index.ts
+++ packages/msgraph-sdk-deviceAppManagement/deviceAppManagement/managedAppPolicies/index.ts
@@ -27,11 +27,11 @@ export interface ManagedAppPoliciesRequestBuilder extends BaseRequestBuilder<Man
      */
      byManagedAppPolicyId(managedAppPolicyId: string) : ManagedAppPolicyItemRequestBuilder;
     /**
-     * List properties and relationships of the targetedManagedAppProtection objects.
+     * List properties and relationships of the windowsInformationProtection objects.
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @returns {Promise<ManagedAppPolicyCollectionResponse>}
      * @throws {ODataError} error when the service returns a 4XX or 5XX status code
-     * @see {@link https://learn.microsoft.com/graph/api/intune-mam-targetedmanagedappprotection-list?view=graph-rest-1.0|Find more info here}
+     * @see {@link https://learn.microsoft.com/graph/api/intune-mam-windowsinformationprotection-list?view=graph-rest-1.0|Find more info here}
      */
      get(requestConfiguration?: RequestConfiguration<ManagedAppPoliciesRequestBuilderGetQueryParameters> | undefined) : Promise<ManagedAppPolicyCollectionResponse | undefined>;
     /**
@@ -43,7 +43,7 @@ export interface ManagedAppPoliciesRequestBuilder extends BaseRequestBuilder<Man
      */
      post(body: ManagedAppPolicy, requestConfiguration?: RequestConfiguration<object> | undefined) : Promise<ManagedAppPolicy | undefined>;
     /**
-     * List properties and relationships of the targetedManagedAppProtection objects.
+     * List properties and relationships of the windowsInformationProtection objects.
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @returns {RequestInformation}
      */
@@ -57,7 +57,7 @@ export interface ManagedAppPoliciesRequestBuilder extends BaseRequestBuilder<Man
      toPostRequestInformation(body: ManagedAppPolicy, requestConfiguration?: RequestConfiguration<object> | undefined) : RequestInformation;
 }
 /**
- * List properties and relationships of the targetedManagedAppProtection objects.
+ * List properties and relationships of the windowsInformationProtection objects.
  */
 export interface ManagedAppPoliciesRequestBuilderGetQueryParameters {
     /**
