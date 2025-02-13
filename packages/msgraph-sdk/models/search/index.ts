42aed30039f185bb3cbea86bc13f279bd80104b6
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-02-13 07:33:58 +0000

    feat(generation): update request builders and models
    Update generated files with build 180153

--- packages/msgraph-sdk/models/search/index.ts
+++ packages/msgraph-sdk/models/search/index.ts
@@ -125,7 +125,7 @@ export interface Bookmark extends Parsable, SearchAnswer {
      */
     state?: AnswerState | null;
     /**
-     * Variations of a bookmark for different countries or devices. Use when you need to show different content to users based on their device, country/region, or both. The date and group settings apply to all variations.
+     * Variations of a bookmark for different countries/regions or devices. Use when you need to show different content to users based on their device, country/region, or both. The date and group settings apply to all variations.
      */
     targetedVariations?: AnswerVariant[] | null;
 }
@@ -488,7 +488,7 @@ export interface Qna extends Parsable, SearchAnswer {
      */
     state?: AnswerState | null;
     /**
-     * Variations of a QnA for different countries or devices. Use when you need to show different content to users based on their device, country/region, or both. The date and group settings apply to all variations.
+     * Variations of a QnA for different countries/regions or devices. Use when you need to show different content to users based on their device, country/region, or both. The date and group settings apply to all variations.
      */
     targetedVariations?: AnswerVariant[] | null;
 }
