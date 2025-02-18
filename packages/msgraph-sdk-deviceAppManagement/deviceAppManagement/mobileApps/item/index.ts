b5056b9dafaa479779615b6f659c33767095a132
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-02-18 11:06:59 +0000

    feat(generation): update request builders and models
    Update generated files with build 180616

--- packages/msgraph-sdk-deviceAppManagement/deviceAppManagement/mobileApps/item/index.ts
+++ packages/msgraph-sdk-deviceAppManagement/deviceAppManagement/mobileApps/item/index.ts
@@ -127,10 +127,10 @@ export interface MobileAppItemRequestBuilder extends BaseRequestBuilder<MobileAp
      */
     get graphWindowsWebApp(): GraphWindowsWebAppRequestBuilder;
     /**
-     * Deletes a androidStoreApp.
+     * Deletes a windowsMobileMSI.
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @throws {ODataError} error when the service returns a 4XX or 5XX status code
-     * @see {@link https://learn.microsoft.com/graph/api/intune-apps-androidstoreapp-delete?view=graph-rest-1.0|Find more info here}
+     * @see {@link https://learn.microsoft.com/graph/api/intune-apps-windowsmobilemsi-delete?view=graph-rest-1.0|Find more info here}
      */
      delete(requestConfiguration?: RequestConfiguration<object> | undefined) : Promise<void>;
     /**
@@ -142,16 +142,16 @@ export interface MobileAppItemRequestBuilder extends BaseRequestBuilder<MobileAp
      */
      get(requestConfiguration?: RequestConfiguration<MobileAppItemRequestBuilderGetQueryParameters> | undefined) : Promise<MobileApp | undefined>;
     /**
-     * Update the properties of a iosLobApp object.
+     * Update the properties of a iosiPadOSWebClip object.
      * @param body The request body
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @returns {Promise<MobileApp>}
      * @throws {ODataError} error when the service returns a 4XX or 5XX status code
-     * @see {@link https://learn.microsoft.com/graph/api/intune-apps-ioslobapp-update?view=graph-rest-1.0|Find more info here}
+     * @see {@link https://learn.microsoft.com/graph/api/intune-apps-iosipadoswebclip-update?view=graph-rest-1.0|Find more info here}
      */
      patch(body: MobileApp, requestConfiguration?: RequestConfiguration<object> | undefined) : Promise<MobileApp | undefined>;
     /**
-     * Deletes a androidStoreApp.
+     * Deletes a windowsMobileMSI.
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @returns {RequestInformation}
      */
@@ -163,7 +163,7 @@ export interface MobileAppItemRequestBuilder extends BaseRequestBuilder<MobileAp
      */
      toGetRequestInformation(requestConfiguration?: RequestConfiguration<MobileAppItemRequestBuilderGetQueryParameters> | undefined) : RequestInformation;
     /**
-     * Update the properties of a iosLobApp object.
+     * Update the properties of a iosiPadOSWebClip object.
      * @param body The request body
      * @param requestConfiguration Configuration for the request such as headers, query parameters, and middleware options.
      * @returns {RequestInformation}
