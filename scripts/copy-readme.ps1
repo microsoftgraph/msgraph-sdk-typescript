91ba7b80d18f93a98d31e4bd8f884eeff035b8b4
rkodev <43806892+rkodev@users.noreply.github.com>
2025-05-19 11:04:27 +0300

    Adds copy-readme

---
+++ scripts/copy-readme.ps1
@@ -0,0 +1,20 @@
+# PowerShell script to copy and modify README.md files in package folders
+
+$scriptPath = Join-Path -Path $PSScriptRoot -ChildPath "README.md"
+$packagesPath = Join-Path -Path $PSScriptRoot -ChildPath "..\packages\.."
+$packageFolders = Get-ChildItem -Path $packagesPath -Directory
+
+foreach ($folder in $packageFolders) {
+    $folderName = $folder.Name
+    $packageName = $folderName.Split('-')[-1]  # Extract the last segment after splitting by '-'
+    $destinationPath = Join-Path -Path $folder.FullName -ChildPath "README.md"
+
+    # Read the content of the template README.md
+    $readmeContent = Get-Content -Path $scriptPath
+
+    # Replace the placeholder with the actual package name
+    $modifiedContent = $readmeContent -replace '\$packageName', $packageName
+
+    # Write the modified content to the destination README.md
+    Set-Content -Path $destinationPath -Value $modifiedContent
+}
