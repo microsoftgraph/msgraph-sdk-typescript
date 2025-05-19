8aba879863590f3a5162b5fc0f5a5ccee3177ee6
rkodev <43806892+rkodev@users.noreply.github.com>
2025-05-19 20:17:07 +0300

    update readme template

--- scripts/copy-readme.ps1
+++ scripts/copy-readme.ps1
@@ -4,6 +4,11 @@ $scriptPath = Join-Path -Path $PSScriptRoot -ChildPath "README.md"
 $packagesPath = Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "packages"
 $packageFolders = Get-ChildItem -Path $packagesPath -Directory
 
+$rootPackage = "msgraph-sdk"
+
+# Exclude the root package folder from the list
+$packageFolders = $packageFolders | Where-Object { $_.Name -ne $rootPackage }
+
 foreach ($folder in $packageFolders) {
     $folderName = $folder.Name
     $packageName = $folderName.Split('-')[-1]  # Extract the last segment after splitting by '-'
@@ -18,3 +23,10 @@ foreach ($folder in $packageFolders) {
     # Write the modified content to the destination README.md
     Set-Content -Path $destinationPath -Value $modifiedContent
 }
+
+# Get the README.md in the parent directory
+$parentReadmePath = Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath "README.md"
+
+#Copy it to "packages/$rootPackage/README.md"
+$destinationPath = Join-Path -Path (Join-Path -Path $packagesPath -ChildPath $rootPackage) -ChildPath "README.md"
+Copy-Item -Path $parentReadmePath -Destination $destinationPath -Force
